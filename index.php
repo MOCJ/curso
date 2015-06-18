79b59deb1e87da39c177c1f5a93077dce436a614
mocj <mariella.ocj@gmail.com>
2015-06-17 20:20:34 -0500

    terminando la estructura del contenido Home

--- index.php
+++ index.php
@@ -10,8 +10,9 @@
 
 <?php include 'inc/header.php';?>
 
-	<div class="workshop-list">
+	<div class="workshop-list">		
 		<article id="w1" class="workshop workshop-left">
+
 			<div class="container">
 				<div class="row">
 					<div class="col-xs-8 main-info">
@@ -21,23 +22,29 @@
 						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
 						consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
 						cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-						proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+						proident, sunt in culpa <a href="#">Pepe Rios</a>qui officia deserunt mollit anim id est laborum.</p>
 						<a href="detail.php " class="btn btn-custom">ME APUNTO</a>
+						<a href="detail.php " class="more-info-link">Quiero mas información</a>
 					</div>
 					<div class="col-xs-3 col-xs-offset-1">
-					
+						<h3 class="hightlighted"><strong>W21</strong>13/14 de abril</h3>
+						<h3 class="hightlighted">29 plazas</h3>
+						<h3 class="hightlighted">$99</h3>					
 					</div>
 				</div>
 			</div>
+
 		</article>
 		<article id="w2" class="workshop workshop-right">
 
 			<div class="container">
 				<div class="row">
-					<div class="col-xs-4">
-
+					<div class="col-xs-3 date-info ">
+						<h3 class="hightlighted"><strong>W21</strong>13/14 de abril</h3>
+						<h3 class="hightlighted">29 plazas</h3>
+						<h3 class="hightlighted">$99</h3>
 					</div>
-					<div class="col-xs-8">
+					<div class="col-xs-8 col-xs-offset-1 main-info">
 						<h3>TITULO WORKSHOP NUMERO 2</h3>
 						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
@@ -45,11 +52,13 @@
 						consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
 						cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
 						proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+						<a href="detail.php " class="more-info-link">Quiero mas información</a>
+						<a href="detail.php " class="btn btn-custom">ME APUNTO</a>
+						
 					</div>
-
 				</div>
-
 			</div>
+
 		</article>
 	</div>
 
