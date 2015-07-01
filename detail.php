c252af82a1d457eb6c842b50e93be316c1b26edf
mocj <mariella.ocj@gmail.com>
2015-07-01 00:07:41 -0500

    Maquetacion en el archivo detail detail: formadores,agenda,indice

--- detail.php
+++ detail.php
@@ -1,29 +1,159 @@
-<div class="container">
+<!DOCTYPE HTML>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<?php include 'inc/head.php';?>
+	<title>DETALLE DE PRIMERA PARTE</title>
+</head>
+<body>
+	<?php include 'inc/header.php'; ?>
 
-	<div class="row">
-		<div class="col-xs-4" class="logo">
-			<center><h1 id= "main-logo" ><a style="color:#FF0000;"href="index.php">FELINA <span style="color:#FF0000;">Producciones</span></a></h1></center>
+	<article id"w1-detail" class="detail">
+		<header>
+			<div class="container">
+				<div class="row">
+					<div class="col-xs-12">
+						<h2>Desarrolla front-end like a boss aprendiendo bootstrap 3</h2>
+					</div>
+				</div>
+			</div>
+		</header>
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-7">
+					<section id="intro">
+						<h3>En este workshop aprenderas...</h3>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+						consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+						cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+						proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+						consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+						cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+						proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+						consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+						cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+						proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+					</section>
+					<section id="trainer-list">
+							<h3>Los formadores</h3>
+							<div class="trainer row">
+								<div class="col-xs-4">
+									<img src="img/1.jpg" alt="personaje 1">
+								</div>
+								<div class="col-xs-8">
+									<h4>Mark zuckerberg</h4>
+									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+									tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+									quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+									consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+									cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+									proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+									<p>Formador 1 para Felina Producciones</p>
+									<a href="https://en.wikipedia.org/wiki/Mark_Zuckerberg" class="trainer-link">Ver hoja de vida</a><a href="https://www.google.com.pe/search?q=imagen+steve+jobs+y+mark+zuckerberg&es_sm=122&source=lnms&tbm=isch&sa=X&ei=32uTVfGYLoemgwSwgILoDQ&ved=0CAcQ_AUoAQ&biw=891&bih=503" class="trainer-link">Ver galeria de imagenes</a>
+								</div>
+							</div>
+							<div class="trainer row">
+								<div class="col-xs-4">
+									<img src="img/2.jpg" alt="personaje 2">
+								</div>
+								<div class="col-xs-8">
+									<h4>Mark zuckerberg</h4>
+									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+									tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+									quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+									consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+									cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+									proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+									<p>Formador 1 para Felina Producciones</p>
+									<a href="https://en.wikipedia.org/wiki/Mark_Zuckerberg" class="trainer-link">Ver hoja de vida</a><a href="https://www.google.com.pe/search?q=imagen+steve+jobs+y+mark+zuckerberg&es_sm=122&source=lnms&tbm=isch&sa=X&ei=32uTVfGYLoemgwSwgILoDQ&ved=0CAcQ_AUoAQ&biw=891&bih=503" class="trainer-link">Ver galeria de imagenes</a>
+								</div>
+							</div>				
+
+					</section>
+
+				</div>
+				<div class="col-xs-5">
+					<section id="registration">
+						<h3>Registro</h3>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+						consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+						cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+						proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+						<div class="registration-badge">
+
+						</div>
+					</section>
+					<section id="agenda">
+						<h3>Agenda del dia</h3>
+						<h4><span>W21</span>Sabado 15 de Diciembre</h4>
+						<dl class="dl-horizontal">
+							<dt>10,80h</dt>
+							<dd>Workshop para 1/4</dd>
+							<dt>11,45h</dt>
+							<dd>Descanso</dd>
+							<dt>12,80h</dt>
+							<dd>Workshop para 2/4</dd>
+							<dt>14,80h</dt>
+							<dd>Comidas</dd>
+							<dt>16,80h</dt>
+							<dd>Workshop para parte 3/4</dd>
+							<dt>17,45h</dt>
+							<dd>Descanso</dd>
+						</dl>
+					</section>
+				</div>
+			</div>
 		</div>
-		<div class="col-xs-8">
-			<b><h2>TeatriPlanet</h2></b>
-		</div>
-	</div>
-	<div class="row">
+		<section id="table-of-contents">
+			<div class="container">
+				<div class="row">
+					<div class="col-xs-12">
+						<h3>Indice de contenidos</h3>
+					</div>
+					<div class="col-xs-4">
+						<h4>1. Framework Front-end</h4>
+						<ul>
+							<li>Item indice primero</li>
+							<li>Item indice segudo</li>
+							<li>Item indice tercero</li>
+						</ul>
+					</div>
+					<div class="col-xs-4">
+						<h4>3. Tercer Punto</h4>
+						<ul>
+							<li>Item indice primero</li>
+							<li>Item indice segudo</li>
+							<li>Item indice tercero</li>
+						</ul>
+					</div>
+					<div class="col-xs-4">
+						<h4>5. Quinto Punto</h4>
+						<ul>
+							<li>Item indice primero</li>
+							<li>Item indice segudo</li>
+							<li>Item indice tercero</li>
+						</ul>
+					</div>
 
-		<ul id="main-menu" class="nav nav-pills">
-			<li class="active"><a href="index.php">INICIO</a></li>
-			<li class="dropdown">
-				<a class="dropdown-toggle" data-toggle="dropdown" href="#">OBRAS TEATRALES <span class="caret"></span></a>
-				<ul class="dropdown-menu" role="menu">
-					<li><a href="detail.php"></a>Detalle A</li>
-					<li><a href="detail.php"></a>Detalle B</li>
-				</ul>
-			</li>
-			<li><a href="#">TEATROS</a></li>
-			<li><a href="#">RESERVAS</a></li>
-			<li><a href="#">CONTACTENOS</a></li>
-		</ul>
+				</div>
+			</div>
+		</section>
+	</article>
 
-	</div>
 
-</div>
+
+
+
+	<?php include 'inc/foot.php';?>
+	<?php include 'inc/footer.php'; ?>
+</body>
+</html>
