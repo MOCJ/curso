2817d34b958ca0e0193f33a480de39966df7b0cb
mocj <mariella.ocj@gmail.com>
2015-07-01 23:43:57 -0500

    se cambio los estilos para el menu

--- inc/header.php
+++ inc/header.php
@@ -11,20 +11,21 @@
 	</div>
 	<div class="row">
 
-		<ul id="main-menu" class="nav nav-pills">
-			<li class="active"><a href="index.php">INICIO</a></li>
-			<li class="dropdown">
-				<a class="dropdown-toggle" data-toggle="dropdown" href="#">OBRAS TEATRALES <span class="caret"></span></a>
-				<ul class="dropdown-menu" role="menu">
-					<li><a href="detail.php">Detalle A</a></li>
-					<li><a href="detail.php">Detalle B</a></li>
-				</ul>
-			</li>
-			<li><a href="#">TEATROS</a></li>
-			<li><a href="#">RESERVAS</a></li>
-			<li><a href="#">CONTACTENOS</a></li>
-		</ul>
-
+		<div class="col-xs-12">
+			<ul id="main-menu" class="nav nav-pills">
+				<li class="active"><a  href="index.php">INICIO</a></li>
+				<li class="dropdown">
+					<a class="dropdown-toggle" data-toggle="dropdown" href="#">OBRAS TEATRALES <span class="caret"></span></a>
+					<ul class="dropdown-menu" role="menu">
+						<li><a href="detail.php">Detalle A</a></li>
+						<li><a href="detail.php">Detalle B</a></li>
+					</ul>
+				</li>
+				<li><a href="#">TEATROS</a></li>
+				<li><a href="#">RESERVAS</a></li>
+				<li><a href="#">CONTACTENOS</a></li>
+			</ul>
+		</div>
 	</div>
 
 </div>
